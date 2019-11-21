61a3f960b3dcbae1015b5d11e2217803f41c69f4
jules Ivanic <jules.ivanic@gmail.com>
2019-11-22 10:04:39 +1100

    Remove useless piece of code

--- benchs/src/main/scala/io/simplesource/benchs/gatling/protocol/Predef.scala
+++ benchs/src/main/scala/io/simplesource/benchs/gatling/protocol/Predef.scala
@@ -3,18 +3,18 @@ package io.simplesource.benchs.gatling.protocol
 import java.time.Duration
 import java.util.concurrent.atomic.AtomicLong
 
-import akka.actor.{ ActorRef, Props }
+import akka.actor.Props
 import io.gatling.core.CoreComponents
 import io.gatling.core.action.Action
 import io.gatling.core.action.builder.ActionBuilder
 import io.gatling.core.akka.BaseActor
 import io.gatling.core.config.GatlingConfiguration
-import io.gatling.core.protocol.{ Protocol, ProtocolComponents, ProtocolKey }
-import io.gatling.core.session.{ Expression, Session }
+import io.gatling.core.protocol.{Protocol, ProtocolComponents, ProtocolKey}
+import io.gatling.core.session.{Expression, Session}
 import io.gatling.core.structure.ScenarioContext
 import io.simplesource.api.CommandAPI.Request
-import io.simplesource.api.{ CommandAPI, CommandError, CommandId }
-import io.simplesource.data.{ FutureResult, Sequence }
+import io.simplesource.api.{CommandAPI, CommandError, CommandId}
+import io.simplesource.data.{FutureResult, Sequence}
 import io.simplesource.kafka.dsl.EventSourcedApp
 
 import scala.concurrent.duration.FiniteDuration
@@ -108,7 +108,7 @@ final case class SimpleSourceProtocolBuilder(
 
 final case class SimpleSourceProtocol(app: EventSourcedApp) extends Protocol
 
-final case class SimpleSourceComponents(protocol: SimpleSourceProtocol, sessions: ActorRef) extends ProtocolComponents {
+final case class SimpleSourceComponents(protocol: SimpleSourceProtocol) extends ProtocolComponents {
   override def onStart: Session => Session = ProtocolComponents.NoopOnStart
   override def onExit: Session => Unit     = ProtocolComponents.NoopOnExit
 }
@@ -134,13 +134,7 @@ object SimpleSourceProtocol {
       override def defaultProtocolValue(configuration: GatlingConfiguration): SimpleSourceProtocol =
         throw new IllegalStateException("Can't provide a default value for SimpleSource")
 
-      override def newComponents(
-        coreComponents: CoreComponents
-      ): SimpleSourceProtocol => SimpleSourceComponents = { protocol =>
-        val sessions: ActorRef =
-          coreComponents.actorSystem.actorOf(SimpleSourceSessions.props(protocol), "simplesourcesessions")
-
-        SimpleSourceComponents(protocol, sessions)
-      }
+      override def newComponents(coreComponents: CoreComponents): SimpleSourceProtocol => SimpleSourceComponents =
+        protocol => SimpleSourceComponents(protocol)
     }
 }
