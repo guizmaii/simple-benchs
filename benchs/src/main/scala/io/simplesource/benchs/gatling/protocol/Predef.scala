4e7b31a1b33708534bf4e1691d08efb4fa562cb2
jules Ivanic <jules.ivanic@gmail.com>
2019-11-22 10:29:38 +1100

    Remove even more useless code

--- benchs/src/main/scala/io/simplesource/benchs/gatling/protocol/Predef.scala
+++ benchs/src/main/scala/io/simplesource/benchs/gatling/protocol/Predef.scala
@@ -1,9 +1,7 @@
 package io.simplesource.benchs.gatling.protocol
 
-import io.gatling.core.CoreComponents
 import io.gatling.core.config.GatlingConfiguration
-import io.gatling.core.protocol.{ Protocol, ProtocolComponents, ProtocolKey }
-import io.gatling.core.session.Session
+import io.gatling.core.protocol.Protocol
 import io.simplesource.kafka.dsl.EventSourcedApp
 
 object Predef extends SimpleSourceDsl
@@ -29,22 +27,3 @@ final case class SimpleSourceProtocolBuilder(
 }
 
 final case class SimpleSourceProtocol(app: EventSourcedApp) extends Protocol
-
-object SimpleSourceProtocol {
-  final val simpleSourceProtocolKey: ProtocolKey[SimpleSourceProtocol, SimpleSourceComponents] =
-    new ProtocolKey[SimpleSourceProtocol, SimpleSourceComponents] {
-      override def protocolClass: Class[Protocol] = classOf[SimpleSourceProtocol].asInstanceOf[Class[Protocol]]
-
-      // TODO Jules: Can we provide defaults for SimpleSource ?
-      override def defaultProtocolValue(configuration: GatlingConfiguration): SimpleSourceProtocol =
-        throw new IllegalStateException("Can't provide a default value for SimpleSource")
-
-      override def newComponents(coreComponents: CoreComponents): SimpleSourceProtocol => SimpleSourceComponents =
-        protocol => SimpleSourceComponents(protocol)
-    }
-}
-
-final case class SimpleSourceComponents(protocol: SimpleSourceProtocol) extends ProtocolComponents {
-  override def onStart: Session => Session = ProtocolComponents.NoopOnStart
-  override def onExit: Session => Unit     = ProtocolComponents.NoopOnExit
-}
