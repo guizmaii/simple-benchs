ee6d1e602213d3213dbd9ad49130779bd6aa547d
jules Ivanic <jules.ivanic@gmail.com>
2019-11-21 14:57:29 +1100

    Update with the latest SimpleSource evolutions, BIS

--- benchs/src/it/scala/io/simplesource/benchs/it/example/Config.scala
+++ benchs/src/it/scala/io/simplesource/benchs/it/example/Config.scala
@@ -2,17 +2,16 @@ package io.simplesource.benchs.it.example
 
 import java.util.Optional
 
-import io.simplesource.example.user.domain.{User, UserCommand, UserEvent, UserKey}
-import io.simplesource.kafka.dsl.AggregateBuilder
-import io.simplesource.kafka.dsl.EventSourcedApp.EventSourcedAppBuilder
+import io.simplesource.example.user.domain.{ User, UserCommand, UserEvent, UserKey }
+import io.simplesource.kafka.client.{ CommandAPIBuilder, EventSourcedClient }
 import io.simplesource.kafka.dsl.KafkaConfig.Builder
+import io.simplesource.kafka.dsl.{ AggregateBuilder, EventSourcedApp }
 import io.simplesource.kafka.serialization.json.JsonAggregateSerdes
 import io.simplesource.kafka.serialization.json.JsonGenericMapper.jsonDomainMapper
 import io.simplesource.kafka.serialization.json.JsonOptionalGenericMapper.jsonOptionalDomainMapper
 import io.simplesource.kafka.util.PrefixResourceNamingStrategy
 
 object Config {
-
   // The following code is copied from here:
   //  - https://github.com/simplesourcing/simplesource-examples/blob/master/examples/user/src/main/java/io/simplesource/example/user/json/UserJsonRunner.java
 
@@ -23,13 +22,13 @@ object Config {
   val aggregateSerdes: JsonAggregateSerdes[UserKey, UserCommand, UserEvent, Optional[User]] =
     new JsonAggregateSerdes(jsonDomainMapper(), jsonDomainMapper(), jsonDomainMapper(), jsonOptionalDomainMapper())
 
-  val app: EventSourcedAppBuilder =
-    new EventSourcedAppBuilder().withKafkaConfig { builder: Builder =>
+  val app: EventSourcedApp =
+    new EventSourcedApp().withKafkaConfig { builder: Builder =>
       builder
-        .withKafkaApplicationId("benchs")
+        .withKafkaApplicationId("benchsApp")
         .withKafkaBootstrap("localhost:9092")
         .build()
-    }.addAggregate { builder: AggregateBuilder[UserKey, UserCommand, UserEvent, Optional[User]] =>
+    }.withAggregate { builder: AggregateBuilder[UserKey, UserCommand, UserEvent, Optional[User]] =>
       builder
         .withName(aggregateName)
         .withSerdes(aggregateSerdes)
@@ -40,4 +39,19 @@ object Config {
         .build()
     }
 
+  val client: EventSourcedClient =
+    new EventSourcedClient().withKafkaConfig { builder =>
+      builder
+        .withKafkaBootstrap("localhost:9092")
+        .build()
+    }
+
+  val commandAPI = client.createCommandApi { builder: CommandAPIBuilder[UserKey, UserCommand] =>
+    builder
+      .withClientId("benchsClient")
+      .withName(aggregateName)
+      .withSerdes(aggregateSerdes)
+      .withResourceNamingStrategy(namingStrategy)
+      .build()
+  }
 }
