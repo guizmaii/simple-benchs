d7bffe259469db31abdd17fddcac3d63ab2f96ee
jules Ivanic <jules.ivanic@gmail.com>
2019-11-22 11:20:34 +1100

    Improve Action generation code

--- benchs/src/it/scala/io/simplesource/benchs/it/example/SimulationExample.scala
+++ benchs/src/it/scala/io/simplesource/benchs/it/example/SimulationExample.scala
@@ -1,36 +1,40 @@
 package io.simplesource.benchs.it.example
 
+import java.util.UUID
+
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
-import io.simplesource.api.{CommandAPI, CommandId}
 import io.simplesource.benchs.gatling.protocol.SimpleSourceProtocolBuilder
 import io.simplesource.data.Sequence
-import io.simplesource.example.user.domain.{UserCommand, UserKey}
+import io.simplesource.example.user.domain.{ UserCommand, UserKey }
 
 import scala.concurrent.duration._
 
 class SimulationExample extends Simulation {
   import Config._
+  import scala.compat.java8.DurationConverters._
 
-  val key       = new UserKey("user2345")
-  val firstName = "Bob"
-  val lastName  = "Dubois"
-
-  val command: UserCommand = new UserCommand.InsertUser(firstName, lastName)
+  val command: () => UserCommand.InsertUser = () => new UserCommand.InsertUser("Bob", "Dubois")
 
   import io.simplesource.benchs.gatling.protocol.Predef._
 
   val simpleSourceProtocol: SimpleSourceProtocolBuilder = simpleSource.withApp(app)
 
-  def newRequest() = new CommandAPI.Request(CommandId.random, key, Sequence.first(), command)
-
   val scn: ScenarioBuilder =
     scenario("Scenario 0") // A scenario is a chain of requests and pauses
-      .exec(stream("Request 1").publishCommand("Command 1")(commandAPI, key, command))
-      .pause(5) // Note that Gatling has recorder real time pauses
-      .exec(stream("Request 2").publishCommand("Command 2")(commandAPI, key, command))
-      .pause(5) // Note that Gatling has recorder real time pauses
-      .exec(stream("Request 3").publishCommand("Command 3")(commandAPI, key, command))
+      .exec(
+        stream("Stream 1")
+          .publishCommand("Command 1: publishCommand")(commandAPI, () => new UserKey(UUID.randomUUID().toString), command, Sequence.first)
+      )
+      .exec(
+        stream("Stream 2").publishAndQueryCommand("Command 2: publishAndQueryCommand")(
+          commandAPI,
+          () => new UserKey(UUID.randomUUID().toString),
+          command,
+          Sequence.first,
+          10.seconds.toJava
+        )
+      )
 
-  setUp(scn.inject(rampUsers(10000) during 2.minutes).protocols(simpleSourceProtocol.build()))
+  setUp(scn.inject(rampUsers(100000) during 30.seconds).protocols(simpleSourceProtocol.build()))
 }
--- benchs/src/main/scala/io/simplesource/benchs/gatling/protocol/Stream.scala
+++ benchs/src/main/scala/io/simplesource/benchs/gatling/protocol/Stream.scala
@@ -1,7 +1,6 @@
 package io.simplesource.benchs.gatling.protocol
 
 import java.time.Duration
-import java.util.concurrent.atomic.AtomicLong
 
 import io.gatling.core.action.Action
 import io.gatling.core.action.builder.ActionBuilder
@@ -11,40 +10,27 @@ import io.simplesource.api.CommandAPI.Request
 import io.simplesource.api.{ CommandAPI, CommandError, CommandId }
 import io.simplesource.data.{ FutureResult, Sequence }
 
-import scala.concurrent.duration.FiniteDuration
-
 final class Stream(actionName: String) {
   def publishCommand[K, C](
     requestName: Expression[String]
-  )(commandAPI: CommandAPI[K, C], key: K, command: C): ActionBuilder =
-    new ActionBuilder {
-      private final val safeSequence = new AtomicLong(0)
+  )(commandAPI: CommandAPI[K, C], key: () => K, command: () => C, sequence: () => Sequence): ActionBuilder =
+    (ctx: ScenarioContext, next: Action) =>
+      new SimpleSourceAction[K, C, Request[K, C], CommandId](actionName, requestName, ctx, next) {
+        override def sendRequest(
+          requestName: String,
+          session: Session,
+          request: CommandAPI.Request[K, C]
+        ): FutureResult[CommandError, CommandId] = commandAPI.publishCommand(request)
 
-      override def build(ctx: ScenarioContext, next: Action): Action =
-        new SimpleSourceAction[K, C, Request[K, C], CommandId](actionName, requestName, ctx, next) {
-          override def sendRequest(
-            requestName: String,
-            session: Session,
-            request: CommandAPI.Request[K, C]
-          ): FutureResult[CommandError, CommandId] = commandAPI.publishCommand(request)
-
-          override def requestParamsGen(): Request[K, C] = {
-            val r = new CommandAPI.Request[K, C](CommandId.random, key, Sequence.position(safeSequence.getAndIncrement()), command)
-
-            logger.warn(s"============== R: $r")
-
-            r
-          }
-        }
-    }
+        override def requestParamsGen(): Request[K, C] =
+          new CommandAPI.Request[K, C](CommandId.random, key(), sequence(), command())
+      }
 
   def queryCommandResult[K, C](
     requestName: Expression[String]
-  )(commandAPI: CommandAPI[K, C], commandId: CommandId, timeout: FiniteDuration): ActionBuilder =
+  )(commandAPI: CommandAPI[K, C], commandId: CommandId, timeout: Duration): ActionBuilder =
     (ctx: ScenarioContext, next: Action) =>
       new SimpleSourceAction[K, C, (CommandId, Duration), Sequence](actionName, requestName, ctx, next) {
-        import scala.compat.java8.DurationConverters._
-
         private final val tupled: ((CommandId, Duration)) => FutureResult[CommandError, Sequence] = (commandAPI.queryCommandResult _).tupled
 
         override def sendRequest(
@@ -53,27 +39,21 @@ final class Stream(actionName: String) {
           request: (CommandId, Duration)
         ): FutureResult[CommandError, Sequence] = tupled(request)
 
-        override def requestParamsGen(): (CommandId, Duration) = (commandId, timeout.toJava)
+        override def requestParamsGen(): (CommandId, Duration) = (commandId, timeout)
       }
 
   def publishAndQueryCommand[K, C](
     requestName: Expression[String]
-  )(commandAPI: CommandAPI[K, C], key: K, command: C, timeout: FiniteDuration): ActionBuilder =
-    new ActionBuilder {
-      private final val safeSequence = new AtomicLong(0)
+  )(commandAPI: CommandAPI[K, C], key: () => K, command: () => C, sequence: () => Sequence, timeout: Duration): ActionBuilder =
+    (ctx: ScenarioContext, next: Action) =>
+      new SimpleSourceAction[K, C, Request[K, C], Sequence](actionName, requestName, ctx, next) {
+        override def sendRequest(
+          requestName: String,
+          session: Session,
+          request: CommandAPI.Request[K, C]
+        ): FutureResult[CommandError, Sequence] = commandAPI.publishAndQueryCommand(request, timeout)
 
-      override def build(ctx: ScenarioContext, next: Action): Action =
-        new SimpleSourceAction[K, C, Request[K, C], Sequence](actionName, requestName, ctx, next) {
-          import scala.compat.java8.DurationConverters._
-
-          override def sendRequest(
-            requestName: String,
-            session: Session,
-            request: CommandAPI.Request[K, C]
-          ): FutureResult[CommandError, Sequence] = commandAPI.publishAndQueryCommand(request, timeout.toJava)
-
-          override def requestParamsGen(): CommandAPI.Request[K, C] =
-            new CommandAPI.Request[K, C](CommandId.random, key, Sequence.position(safeSequence.getAndIncrement()), command)
-        }
-    }
+        override def requestParamsGen(): CommandAPI.Request[K, C] =
+          new CommandAPI.Request[K, C](CommandId.random, key(), sequence(), command())
+      }
 }
