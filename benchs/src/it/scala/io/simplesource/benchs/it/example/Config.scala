80028bc69da1a3b72f11b152a21bd3c6872b9906
jules Ivanic <jules.ivanic@gmail.com>
2019-11-22 15:26:16 +1100

    Add some Avro tests

--- benchs/src/it/scala/io/simplesource/benchs/it/example/Config.scala
+++ benchs/src/it/scala/io/simplesource/benchs/it/example/Config.scala
@@ -3,56 +3,82 @@ package io.simplesource.benchs.it.example
 import java.util.Optional
 
 import io.simplesource.api.CommandAPI
+import io.simplesource.example.user.avro.UserAvroMappers.{ aggregateMapper, commandMapper, eventMapper, keyMapper }
 import io.simplesource.example.user.domain.{ User, UserCommand, UserEvent, UserKey }
+import io.simplesource.kafka.api.{ AggregateSerdes, ResourceNamingStrategy }
 import io.simplesource.kafka.client.{ CommandAPIBuilder, EventSourcedClient }
 import io.simplesource.kafka.dsl.KafkaConfig.Builder
 import io.simplesource.kafka.dsl.{ AggregateBuilder, EventSourcedApp }
+import io.simplesource.kafka.serialization.avro.AvroAggregateSerdes
 import io.simplesource.kafka.serialization.json.JsonAggregateSerdes
-import io.simplesource.kafka.serialization.json.JsonGenericMapper.jsonDomainMapper
-import io.simplesource.kafka.serialization.json.JsonOptionalGenericMapper.jsonOptionalDomainMapper
 import io.simplesource.kafka.util.PrefixResourceNamingStrategy
 
 object Config {
-  // The following code is copied from here:
-  //  - https://github.com/simplesourcing/simplesource-examples/blob/master/examples/user/src/main/java/io/simplesource/example/user/json/UserJsonRunner.java
+  // The following code is inspired by:
+  //  - https://github.com/simplesourcing/simplesource-examples/blob/master/examples/user/src/main/java/io/simplesource/example/user
 
-  val aggregateName: String = "benchs-aggregate"
+  private val bootstrapServers = "localhost:9092"
+  private val schemaRegistry   = "localhost:8081"
 
-  val namingStrategy: PrefixResourceNamingStrategy = new PrefixResourceNamingStrategy("user_json_")
+  private val jsonAggregateName: String                                                                 = "json-benchs-aggregate"
+  private val jsonNamingStrategy: PrefixResourceNamingStrategy                                          = new PrefixResourceNamingStrategy("user_json_")
+  private val jsonAggregateSerdes: JsonAggregateSerdes[UserKey, UserCommand, UserEvent, Optional[User]] = new JsonAggregateSerdes()
 
-  val aggregateSerdes: JsonAggregateSerdes[UserKey, UserCommand, UserEvent, Optional[User]] =
-    new JsonAggregateSerdes(jsonDomainMapper(), jsonDomainMapper(), jsonDomainMapper(), jsonOptionalDomainMapper())
+  private val avroAggregateName: String                        = "avro-benchs-aggregate"
+  private val avroNamingStrategy: PrefixResourceNamingStrategy = new PrefixResourceNamingStrategy("user_avro_");
+  private val avroAggregateSerdes: AvroAggregateSerdes[UserKey, UserCommand, UserEvent, Optional[User]] =
+    new AvroAggregateSerdes(
+      keyMapper,
+      commandMapper,
+      eventMapper,
+      aggregateMapper,
+      schemaRegistry,
+      false,
+      io.simplesource.example.user.avro.api.User.SCHEMA$
+    )
 
-  val app: EventSourcedApp =
-    new EventSourcedApp().withKafkaConfig { builder: Builder =>
-      builder
-        .withKafkaApplicationId("benchsApp")
-        .withKafkaBootstrap("localhost:9092")
-        .build()
-    }.withAggregate { builder: AggregateBuilder[UserKey, UserCommand, UserEvent, Optional[User]] =>
-      builder
-        .withName(aggregateName)
-        .withSerdes(aggregateSerdes)
-        .withResourceNamingStrategy(namingStrategy)
-        .withInitialValue(_ => Optional.empty())
-        .withAggregator(UserEvent.getAggregator)
-        .withCommandHandler(UserCommand.getCommandHandler)
-        .build()
-    }
+  private def appAndClient(
+    applicationId: String,
+    clientId: String,
+    aggregateName: String,
+    namingStrategy: ResourceNamingStrategy,
+    serdes: AggregateSerdes[UserKey, UserCommand, UserEvent, Optional[User]]
+  ): (EventSourcedApp, CommandAPI[UserKey, UserCommand]) = {
+    val app: EventSourcedApp =
+      new EventSourcedApp().withKafkaConfig { builder: Builder =>
+        builder
+          .withKafkaApplicationId(applicationId)
+          .withKafkaBootstrap(bootstrapServers)
+          .build()
+      }.withAggregate { builder: AggregateBuilder[UserKey, UserCommand, UserEvent, Optional[User]] =>
+        builder
+          .withName(aggregateName)
+          .withSerdes(serdes)
+          .withResourceNamingStrategy(namingStrategy)
+          .withInitialValue(_ => Optional.empty())
+          .withAggregator(UserEvent.getAggregator)
+          .withCommandHandler(UserCommand.getCommandHandler)
+          .build()
+      }
 
-  val client: EventSourcedClient =
-    new EventSourcedClient().withKafkaConfig { builder =>
-      builder
-        .withKafkaBootstrap("localhost:9092")
-        .build()
-    }
+    val commandAPI: CommandAPI[UserKey, UserCommand] =
+      new EventSourcedClient()
+        .withKafkaConfig(_.withKafkaBootstrap(bootstrapServers).build())
+        .createCommandAPI { builder: CommandAPIBuilder[UserKey, UserCommand] =>
+          builder
+            .withClientId(clientId)
+            .withName(aggregateName)
+            .withSerdes(serdes)
+            .withResourceNamingStrategy(namingStrategy)
+            .build()
+        }
 
-  val commandAPI: CommandAPI[UserKey, UserCommand] = client.createCommandAPI { builder: CommandAPIBuilder[UserKey, UserCommand] =>
-    builder
-      .withClientId("benchsClient")
-      .withName(aggregateName)
-      .withSerdes(aggregateSerdes)
-      .withResourceNamingStrategy(namingStrategy)
-      .build()
+    (app, commandAPI)
   }
+
+  def jsonAppAndClient: (EventSourcedApp, CommandAPI[UserKey, UserCommand]) =
+    appAndClient("json-user-benchs-app", "json-user-benchs-client", jsonAggregateName, jsonNamingStrategy, jsonAggregateSerdes)
+
+  def avroAppAndClient: (EventSourcedApp, CommandAPI[UserKey, UserCommand]) =
+    appAndClient("avro-user-benchs-app", "avro-user-benchs-client", avroAggregateName, avroNamingStrategy, avroAggregateSerdes)
 }
--- benchs/src/it/scala/io/simplesource/benchs/it/example/JsonSimulationExample.scala
+++ benchs/src/it/scala/io/simplesource/benchs/it/example/JsonSimulationExample.scala
@@ -6,36 +6,39 @@ import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 import io.simplesource.benchs.gatling.protocol.SimpleSourceProtocolBuilder
 import io.simplesource.data.Sequence
-import io.simplesource.example.user.domain.{ UserCommand, UserKey }
+import io.simplesource.example.user.domain.{UserCommand, UserKey}
 
 import scala.concurrent.duration._
 
-class SimulationExample extends Simulation {
+class JsonSimulationExample extends Simulation {
   import Config._
-  import scala.compat.java8.DurationConverters._
 
   val newRandomUserKey: () => UserKey               = () => new UserKey(UUID.randomUUID().toString)
   val constantCommand: () => UserCommand.InsertUser = () => new UserCommand.InsertUser("Bob", "Dubois")
 
   import io.simplesource.benchs.gatling.protocol.Predef._
 
+  val (app, commandAPI) = jsonAppAndClient
+
   val simpleSourceProtocol: SimpleSourceProtocolBuilder = simpleSource.withApp(app)
 
   val scn: ScenarioBuilder =
-    scenario("Scenario 0") // A scenario is a chain of requests and pauses
-    /*.exec(
-        stream("Stream 1")
-          .publishCommand("Command 1: publishCommand")(commandAPI, newrandokmUserKey, constantCommand, Sequence.first)
-      )*/
+    scenario("JSON Scenario 0") // A scenario is a chain of requests and pauses
       .exec(
-        stream("Stream 2").publishAndQueryCommand("Command 2: publishAndQueryCommand")(
+        stream("JSON Stream 1")
+          .publishCommand("JSON Command 1: publishCommand")(commandAPI, newRandomUserKey, constantCommand, Sequence.first)
+      )
+      .exec {
+        import scala.compat.java8.DurationConverters._
+
+        stream("JSON Stream 2").publishAndQueryCommand("JSON Command 2: publishAndQueryCommand")(
           commandAPI,
           newRandomUserKey,
           constantCommand,
           Sequence.first,
           20.seconds.toJava
         )
-      )
+      }
 
   setUp(scn.inject(rampUsers(100000) during 10.minutes).protocols(simpleSourceProtocol.build()))
 }
