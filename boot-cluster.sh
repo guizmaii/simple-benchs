8a08c061fe99aa2013ac4335b2d7724c61cf3816
jules Ivanic <jules.ivanic@gmail.com>
2019-11-25 10:39:55 +1100

    Try( Fix CI )

--- boot-cluster.sh
+++ boot-cluster.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
+# https://codewithhugo.com/just-enough-bash-to-be-dangerous/
+set -e
+set -o pipefail
+set -x
+
 docker-compose down
 rm -rf /tmp/kafka-streams/
 docker system prune --volumes -f
-docker-compose up
+
+# https://stackoverflow.com/a/39296583
+if [[ -z "${CI}" ]]; then
+  docker-compose up > /dev/null
+else
+  docker-compose up
+fi
